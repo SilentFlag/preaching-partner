4358a6357595e6cf44d5e61fbfcad1af025855ec
Ethan Hayward <hayward.ethan@icloud.com>
2026-05-14 11:06:26 +1200

    starting rewrite

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,61 +1,67 @@
 pub mod datatypes;
-pub mod networking;
-pub mod setup;
+pub mod user_input;
 
-use tokio::sync::{mpsc, oneshot, broadcast};
-use datatypes::{WsState, WsRequest, ClientPayload, ServerPayload, FrontendReponse};
-use tauri::{AppHandle, Emitter, State};
+use datatypes::{WsEvent, WsRequest, WsState};
+use futures_util::StreamExt;
+use tokio::sync::{broadcast, mpsc};
 
-#[tauri::command]
-async fn login(app_handle: AppHandle, state: State<'_, WsState>, username: String, password: String) -> Result<(), String> {
-    let (tx, rx) = oneshot::channel();
-    let msg = ClientPayload::Login {name: username, password: password};
-    println!("{:?}", &msg);
-    let request = WsRequest {
-        payload: msg,
-        response_tx: tx,
-    };
+use tokio_tungstenite::connect_async;
 
-    let sent_mess = state.request_tx.send(request).await.map_err(|e| e.to_string());
-    println!("message sent something {:?}", sent_mess);
+async fn core_loop(
+    mut request_rx: mpsc::Receiver<WsRequest>,
+    _event_tx: broadcast::Sender<WsEvent>,
+) {
+    // Create Connection
+    let url = String::from("ws://127.0.0.1:9001");
 
-    let response = rx.await.map_err(|e| e.to_string());
-    println!("Recieved message to login function: {:?}", response);
-    // TODO: handle error case
-    let success = if let Ok(msg) = response {
-        match msg.payload {
-            ServerPayload::ConfirmLogin{success, ..} => {
-                success
+    let (ws_stream, _) = connect_async(&url).await.expect("Failed to connect");
+
+    println!("Connected to server");
+
+    let (mut _write, mut read) = ws_stream.split();
+
+    // TODO: RUSTLS ENCRYPTION
+
+    // Open database
+    // let my_pool_option = SqliteConnectOptions::from_str("sqlite://../database/data.db"); // ----------------- ERROR ------------
+    // let conn = match my_pool_option {
+    //     Ok(my_pool_option) => {
+    //         let my_pool_option = my_pool_option.journal_mode(sqlx::sqlite::SqliteJournalMode::Wal);
+    //         let conn = SqlitePool::connect_with(my_pool_option).await;
+    //         match conn {
+    //             Ok(conn) => conn,
+    //             Err(error) => {
+    //                 panic!("Connection to database failed: {:?}", error);
+    //             }
+    //         }
+    //     }
+    //     Err(error) => {
+    //         panic!("Database Options Failed: {:?}", error);
+    //     }
+    // };
+    // let db = &conn;
+
+    // Loop to check for messages and stuff
+    loop {
+        tokio::select! {
+            // handle io messages
+            Some(_req) = request_rx.recv() => {
+                // TODO: Check database for existing data before sending request
+
+                // handle outgoing ws message
+
             }
-            _ => {
-                // TODO: Handle this case
-                println!("Unexpected message from server");
-                false
+
+            // Handle incoming messages
+            Some(_msg) = read.next() => {
+                // TODO: messages from the server
             }
         }
-    } else {
-        // TODO: handle recieving message fail
-        false
-    };
-
-    // TODO: Send message to frontend (webview)
-    let payload = FrontendReponse::ConfirmLogin {
-        success,
-        name: "Default".to_string(), // or Some(token_string)
-    };
-
-    // TODO: Handle the error rather than crash with ?
-    app_handle
-        .emit("login-result", payload)
-        .map_err(|e| e.to_string())?;
-    
-
-    Ok(())
+    }
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-
     let (request_tx, request_rx) = mpsc::channel(32);
     let (event_tx, _) = broadcast::channel(32);
 
@@ -64,14 +70,15 @@ pub fn run() {
         event_tx: event_tx.clone(),
     };
 
+    // Spawn backend loop
     tauri::async_runtime::spawn(async {
-        networking::connect_to_server(request_rx, event_tx).await;
+        core_loop(request_rx, event_tx).await;
     });
 
     tauri::Builder::default()
         .manage(ws_state)
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![login])
+        .invoke_handler(tauri::generate_handler![user_input::login])
         .build(tauri::generate_context!())
         .expect("error while building tauri application")
         .run(|_app_handle, event| {
@@ -85,4 +92,3 @@ pub fn run() {
             }
         });
 }
-
--- src-tauri/src/user_input/mod.rs
+++ src-tauri/src/user_input/mod.rs
@@ -1,14 +1,31 @@
+use crate::datatypes::{ClientPayload, FrontendReponse, ServerPayload, WsRequest, WsState};
+
+use tauri::{AppHandle, Emitter, State};
+use tokio::sync::oneshot;
+
 #[tauri::command]
-async fn login(app_handle: AppHandle, state: State<'_, WsState>, username: String, password: String) -> Result<(), String> {
+pub async fn login(
+    app_handle: AppHandle,
+    state: State<'_, WsState>,
+    username: String,
+    password: String,
+) -> Result<(), String> {
     let (tx, rx) = oneshot::channel();
-    let msg = ClientPayload::Login {name: username, password: password};
+    let msg = ClientPayload::Login {
+        name: username,
+        password: password,
+    };
     println!("{:?}", &msg);
     let request = WsRequest {
         payload: msg,
         response_tx: tx,
     };
 
-    let sent_mess = state.request_tx.send(request).await.map_err(|e| e.to_string());
+    let sent_mess = state
+        .request_tx
+        .send(request)
+        .await
+        .map_err(|e| e.to_string());
     println!("message sent something {:?}", sent_mess);
 
     let response = rx.await.map_err(|e| e.to_string());
@@ -16,9 +33,7 @@ async fn login(app_handle: AppHandle, state: State<'_, WsState>, username: Strin
     // TODO: handle error case
     let success = if let Ok(msg) = response {
         match msg.payload {
-            ServerPayload::ConfirmLogin{success, ..} => {
-                success
-            }
+            ServerPayload::ConfirmLogin { success, .. } => success,
             _ => {
                 // TODO: Handle this case
                 println!("Unexpected message from server");
@@ -40,7 +55,6 @@ async fn login(app_handle: AppHandle, state: State<'_, WsState>, username: Strin
     app_handle
         .emit("login-result", payload)
         .map_err(|e| e.to_string())?;
-    
 
     Ok(())
 }
